568c2e8e6429f7ab3a0789860d7235957cbbf63b
volkangoksel <78281870+volkangoksel@users.noreply.github.com>
2026-02-20 23:29:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def get_data():
 
 # --- 3. DASHBOARD GÖRÜNÜMÜ ---
 st.set_page_config(page_title="WBA KPI Dashboard", layout="wide")
-st.title("🚀 WBA KPI Performans Dasboard")
+st.title("🚀 WBA KPI Takip Dasboard")
 
 df = get_data()
 
